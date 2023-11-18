d163b2b2ebbe5708c5291acb4050c6a1bd89a23e
Arun <arun.saxena@csuglobal.edu>
2023-11-17 23:16:48 -0800

    adding CT01 assignments

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SchoolPython
+- I am using this for my school projects
+## Index
+
+| Programs      | Purpose       | Date  |
+| ------------- |:-------------:| -----:|
+| CT01_part1    | Add and Subtract| 11/17/2023|
+| CT01_part2    | Multiply and Divide| 11/17/2023|
---
+++ Sem1_CT_01/CT01_Part2.py
@@ -0,0 +1,9 @@
+#Input from user
+num1 = float(input("enter first number: "))
+num2 = float(input("enter second number: "))
+#Do the calculations (multiplication and division)
+multiply = round(num1*num2,2)
+divide = round(num1/num2,2)
+#output
+print("Multiplication:",num1,"*",num2,"=",multiply)
+print("Division:",num1,"/",num2,"=",divide)
