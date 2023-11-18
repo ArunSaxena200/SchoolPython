1363671242aa8c7d812950edb447d1d9bfa3046d
Arun <arun.saxena@csuglobal.edu>
2023-11-17 23:21:53 -0800

    indentation

--- Sem1_CT_01/CT01_Part1.py
+++ Sem1_CT_01/CT01_Part1.py
@@ -1,9 +1,11 @@
 #Input from user
 num1 = float(input("enter first number: "))
 num2 = float(input("enter second number: "))
+
 #Do the calculations (sum and diff)
 sum = round(num1+num2,2)
 diff = round(num1-num2,2)
+
 #output
 print("Sum:",num1,"+",num2,"=",sum)
 print("Diff:",num1,"-",num2,"=",diff)
